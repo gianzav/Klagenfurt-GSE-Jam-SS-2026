8114f33262bcadefbd429f34722c47191e39979b
gianzav <gianlucagianluca18@gmail.com>
2026-05-30 12:09:27 +0200

    Button accepts callbacks to run when clicked

--- src/button.lua
+++ src/button.lua
@@ -8,9 +8,9 @@ function Button.new(label, x, y, width, height)
     self.y = y
     self.width = width
     self.height = height
+    self.callbacks = {}
     return self
 end
-
 
 function Button:draw()
     love.graphics.setColor(0.8, 0.8, 0.8) -- Light gray color
@@ -21,4 +21,15 @@ end
 
 function Button:isClicked(mx, my)
     return mx >= self.x and mx <= (self.x + self.width) and my >= self.y and my <= (self.y + self.height)
+end
+
+function Button:registerCallback(callback)
+   -- Add a callback to be run when the mouse is clicked
+   table.insert(self.callbacks, callback)
+end
+
+function Button:runCallbacks()
+   for k,callback in pairs(self.callbacks) do
+      callback(self)
+   end
 end
