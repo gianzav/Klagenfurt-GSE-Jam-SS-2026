80eee802d5f685caa657881ef8df265a7c7dedc1
gianzav <gianlucagianluca18@gmail.com>
2026-05-30 17:03:20 +0200

    Added basic save,edit,play buttons

--- src/game.lua
+++ src/game.lua
@@ -16,11 +16,9 @@ function love.load()
    gridPixelWidth, gridPixelHeight = gridWidth*cellSize, gridHeigth*cellSize   
    gridXOffset, gridYOffset = canvasWidth/2-gridPixelWidth/2, canvasHeight/2-gridPixelHeight/2
 
-   -- Game objects
-   saveButton = Button.new("Save", 10, 380, 200, 50)
 
-   
-   saveButton:registerCallback(function (self) self.label = "Saved!" end)
+
+   -- color buttons for edit mode
    
    grid = Grid.new(gridXOffset, gridYOffset, 8, 8, 50)
    ball = Ball.new({r=1, g=1, b=1, a=1}, 50, 50, 16)
@@ -40,64 +38,113 @@ function love.load()
    end
    
    balls = {}
-   possibleBallColors = {
+   colors = {
       {r=1, g=0, b=0, a=1}, -- red
       {r=0, g=1, b=0, a=1}, -- green
       {r=0, g=0, b=1, a=1}, -- red
    }
 
+   -- timer data
    timeElapsed = 0
    time = 0
    lastTime = 0
+   -- time in seconds after which a new ball spawns
+   ballSpawnThreshold = 1
 
-   ballSpawnThreshold = 0.5
+
+   -- gameMode may be one of {"play", "pause", "edit"}
+   gameMode = "edit"
+   -- Game objects
+
+   -- Buttons
+   saveButton = Button.new("Save", 0,   550, 200, 50)
+   editButton = Button.new("Edit", 200, 550, 200, 50)
+   playButton = Button.new("Play", 400, 550, 200, 50)
+   playButton:registerCallback(function (self)
+	 if gameMode ~= "play" then
+	    gameMode = "play"
+	    self.label = "Pause"
+	 else
+	    gameMode = "pause"
+	    self.label = "Play"
+	 end
+   end)
+
+   editButton:registerCallback(function (self)
+	 gameMode = "edit"
+	 balls = {}
+   end)
+
+   buttons = {saveButton, editButton, playButton}
+
+
+   -- colorButtons for edit mode
+   white = {r=1,g=1,b=1,a=1}
+   editingColor = white
+   colorButtons = {}
 end
 
 function love.update(dt)
-   for _,column in pairs(fallColumns) do
-      column:update(dt)
-   end
+   if gameMode == "play" then
+      for _,column in pairs(fallColumns) do
+	 column:update(dt)
+      end
 
-   -- generate a ball
-   time = love.timer.getTime()
-   
-   if lastTime then
-      timeElapsed = timeElapsed + (time - lastTime)
-   else
-      timeElapsed = 0
-   end
-   
-   if timeElapsed >= ballSpawnThreshold then
-      column = math.random(1,#fallColumns)
-      table.insert(balls, generateBall(fallColumns[column],
-				       (column-1)*50+gridXOffset,
-				       gridYOffset-60,
-				       possibleBallColors))
-      timeElapsed = 0
-   end
+      -- generate a ball
+      time = love.timer.getTime()
+      
+      if lastTime then
+	 timeElapsed = timeElapsed + (time - lastTime)
+      else
+	 timeElapsed = 0
+      end
+      
+      if timeElapsed >= ballSpawnThreshold then
+	 column = math.random(1,#fallColumns)
+	 table.insert(balls, generateBall(fallColumns[column],
+					  (column-1)*50+gridXOffset,
+					  gridYOffset-60,
+					  colors))
+	 timeElapsed = 0
+      end
 
-   lastTime = time
+      lastTime = time
+      
+   -- Mouse cursor should be visible in edit mode
+   elseif gameMode == "edit" then
+      love.mouse.setVisible(true)
+   end
 end
 
 function love.mousepressed(x, y, button, istouch, presses)
    if button == 1 then
-      if saveButton:isClicked(x, y) then
-	 saveButton:runCallbacks()
+      for _,button in pairs(buttons) do
+	 if button:isClicked(x,y) then
+	    button:runCallbacks()
+	 end
       end
 
-      for k,ball in pairs(balls) do
-	 if x >= ball:getX()-25 and x <= ball:getX()+25 and
-	    y >= ball:getY()-25 and y <= ball:getY()+25 then
-	    
-	    grid:selectCell(x,y,ball.color)
-	    balls[k] = nil
+      if gameMode == "play" then
+	 for k,ball in pairs(balls) do
+	    if x >= ball:getX()-25 and x <= ball:getX()+25 and
+	       y >= ball:getY()-25 and y <= ball:getY()+25 then
+	       
+	       grid:selectCell(x,y,ball.color)
+	       balls[k] = nil
+	    end
 	 end
+      elseif gameMode == "edit" then
+	 print("ciao")
       end
    end
 end
 
+
 function love.draw()
-   -- saveButton:draw()
+   editButton:draw()
+   playButton:draw()
+   saveButton:draw()
+   
    grid:draw()
    
    -- Draw the circle.
@@ -105,26 +152,8 @@ function love.draw()
      ball:draw()
    end
 
-   -- Draw image on mouse cursor
-   love.graphics.draw(cursorImage, love.mouse.getX()-cursorImage:getHeight()/2, love.mouse.getY()-cursorImage:getWidth()/2)
-end
-
-function generateBall(world, x, y, colors)
-   local ballRadius = 25
-   
-   -- Create a Body for the circle
-   body = love.physics.newBody(world, x+ballRadius, y, "dynamic")
-   
-   -- Attatch a shape to the body.
-   circle_shape = love.physics.newCircleShape(0,0,25)
-   
-   -- Create fixture between body and shape
-   fixture = love.physics.newFixture(body, circle_shape)
-
-   -- Calculate the mass of the body based on attatched shapes.
-   -- This gives realistic simulations.
-   body:setMassData(circle_shape:computeMass( 1 ))
-   randomColor = math.random(1, #colors)
-   ball = Ball.new(colors[randomColor], 25, body)
-   return ball
+   if gameMode == "play" then
+      -- Draw image on mouse cursor
+      love.graphics.draw(cursorImage, love.mouse.getX()-cursorImage:getHeight()/2, love.mouse.getY()-cursorImage:getWidth()/2)
+   end
 end
