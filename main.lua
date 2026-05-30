0864d44c79f5172c13140977cdc8f7b1b1b6fb22
gianzav <gianlucagianluca18@gmail.com>
2026-05-30 12:12:11 +0200

    Added proper module management code

--- main.lua
+++ main.lua
@@ -1,1 +1,15 @@
-require("./src/game")
+function addRelPath(dir)
+  local spath =
+      debug.getinfo(1,'S').source
+        :sub(2)
+        :gsub("^([^/])","./%1")
+        :gsub("[^/]*$","")
+  dir=dir and (dir.."/") or ""
+  spath = spath..dir
+  package.path = spath.."?.lua;"
+               ..spath.."?/init.lua"
+               ..package.path
+end
+
+addRelPath("./src")
+require("game")
