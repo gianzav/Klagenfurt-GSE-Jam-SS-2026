51dd87a64d020885a02e8fb75b38bcbda06b6b8e
gianzav <gianlucagianluca18@gmail.com>
2026-05-30 12:53:26 +0200

    Added image as cursor

--- src/game.lua
+++ src/game.lua
@@ -5,9 +5,10 @@ require("ball")
 function love.load()
    -- General config
    canvasWidth, canvasHeight = 800, 600
-   canvas = love.graphics.newCanvas(canvasWidth, canvasHeight)
-   love.graphics.setCanvas()
-  
+   -- canvas = love.graphics.newCanvas(canvasWidth, canvasHeight)
+   -- love.graphics.setCanvas()
+
+   love.window.setMode(800, 600, {resizable=false, vsync=0, minwidth=800, minheight=600})
    gridDim = 8
    cellSize = 50 -- Width and height of cells.
    gridWidth, gridHeigth = 8,8
@@ -15,11 +16,19 @@ function love.load()
    gridXOffset, gridYOffset = canvasWidth/2-gridPixelWidth/2, canvasHeight/2-gridPixelHeight/2
 
    -- Game objects
-   saveButton = Button.new("Save", 100, 100, 200, 50)
+   saveButton = Button.new("Save", 10, 380, 200, 50)
+
+   
    saveButton:registerCallback(function (self) self.label = "Saved!" end)
+   
    grid = Grid.new(gridXOffset, gridYOffset, 8, 8, 50)
    ball = Ball.new({r=1, g=1, b=1, a=1}, 50, 50, 16)
+
+   cursorImage = love.graphics.newImage("src/assets/elf192.192.png", {dpiscale=2})
 end
+
+
+
 
 function love.mousepressed(x, y, button, istouch, presses)
    if button == 1 then
@@ -28,6 +37,8 @@ function love.mousepressed(x, y, button, istouch, presses)
       end
 
       grid:selectCell(x,y)
+
+      -- show elf on the clicked point
    end
 end
 
@@ -35,4 +46,7 @@ function love.draw()
    saveButton:draw()
    grid:draw()
    ball:draw()
+   
+   -- Draw image on mouse cursor
+   love.graphics.draw(cursorImage, love.mouse.getX()-cursorImage:getHeight()/2, love.mouse.getY()-cursorImage:getWidth()/2)
 end
